ac6d0ab972d40f63eaf7c2ac59aed4657ec4199a
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-05-26 13:24:22 -0700

    Styling for chips/checkers

--- client/src/components/Square/Square.js
+++ client/src/components/Square/Square.js
@@ -3,7 +3,13 @@ import React from "react";
 const Square = props => {
   return (
     <div className="square">
-      <div className={`circle ${props.color}`} />
+      <div className={`circle ${props.color}`}>
+        <div className={`inner-circle ${props.color}`}>
+          <div className={`ridge ${props.color}`}>
+            <div className={`inner-ridge ${props.color}`} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
