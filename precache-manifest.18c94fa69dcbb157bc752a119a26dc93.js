3698020714e5fc05f26983ee3fb29cc3cab036fa
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-05-28 17:50:30 -0400

    Updates

--- precache-manifest.18c94fa69dcbb157bc752a119a26dc93.js
+++ precache-manifest.18c94fa69dcbb157bc752a119a26dc93.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a34ad4fe15b06cb0c6d40a987bbd57b7",
+    "revision": "3b809abf70a3aaa9bd384f460a3bc0ee",
     "url": "/connect-four/index.html"
   },
   {
-    "revision": "336de3447f6846aff1d7",
-    "url": "/connect-four/static/css/main.061061ef.chunk.css"
+    "revision": "67ed809af08f03131a17",
+    "url": "/connect-four/static/css/main.82e0406b.chunk.css"
   },
   {
     "revision": "8a57eb1954f629ca12b7",
     "url": "/connect-four/static/js/2.1b2f06e7.chunk.js"
   },
   {
-    "revision": "336de3447f6846aff1d7",
-    "url": "/connect-four/static/js/main.2c35ec6a.chunk.js"
+    "revision": "67ed809af08f03131a17",
+    "url": "/connect-four/static/js/main.38b1d171.chunk.js"
   },
   {
     "revision": "5f78906a733342f2d383",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/connect-four/precache-manifest.d0939e53bcd412f4f437efda29feff15.js"
+  "/connect-four/precache-manifest.18c94fa69dcbb157bc752a119a26dc93.js"
 );
 
 self.addEventListener('message', (event) => {
